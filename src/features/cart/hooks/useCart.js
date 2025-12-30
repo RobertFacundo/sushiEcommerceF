58f61f714c7b602fbcb7ebc094ef6ad1351a13ca
Facundo Robert <robert.facundo@hotmail.com>
2025-12-30 15:23:43 -0300

    cartservice.js

--- src/features/cart/hooks/useCart.js
+++ src/features/cart/hooks/useCart.js
@@ -5,6 +5,7 @@ export const useCart = () => {
     return useQuery({
         queryKey: ['cart'],
         queryFn: cartService.getCart,
-        staleTime: 1000 * 60 * 2
+        staleTime: 1000 * 60 * 2,
+        keepPreviousData: true
     });
 };
--- src/features/cart/services/cartServices.js
+++ src/features/cart/services/cartServices.js
@@ -6,15 +6,16 @@ export const cartService = {
         const cartId = getCartId();
         const { data } = await api.get(`/cart?cartId=${cartId}`);
 
-        console.log(data, 'log del get cart service')
-        if (data.cart?.cartId) setCartId(data.cart.cartId);
+        // console.log(data, 'log del get cart service')
+        // if (data.cart?.cartId) setCartId(data.cart.cartId);
+        console.log(data, 'log del cartservice')
 
-        return data.cart;
+        return data;
     },
     addItem: async ({ productId, quantity = 1 }) => {
         const cartId = getCartId();
         const { data } = await api.post('/cart/items', { productId, quantity, cartId });
-        if (data.cart?.cartId) setCartId(data.cart.cartId);
+        if (data?.cartId) setCartId(data?.cartId);
 
         return data.cart;
     },
@@ -27,7 +28,7 @@ export const cartService = {
     clearCart: async () => {
         const cartId = getCartId();
         const { data } = await api.delete(`/cart/items?cartId=${cartId}`);
-        if (data.cart?.cartId) setCartId(data.cart.cartId);
+        if (data?.cartId) setCartId(data?.cartId);
         
         return data.cart;
     },
